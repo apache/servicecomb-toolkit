c8658ec773a72e96f84138279253cd70a7d6949d
MabinGo <bin.ma@huawei.com>
2019-05-20 20:20:24 +0800

    Add feature of contract generating document
    Signed-off-by: MabinGo <bin.ma@huawei.com>

---
+++ doc-generator/src/main/java/org/apache/servicecomb/docgen/ContractsAsciidocGenerator.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.servicecomb.docgen;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Attributes;
+import org.asciidoctor.AttributesBuilder;
+import org.asciidoctor.OptionsBuilder;
+import org.asciidoctor.SafeMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Ordering;
+
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.swagger.models.Swagger;
+import io.swagger.models.parameters.Parameter;
+
+
+public class ContractsAsciidocGenerator implements DocGenerator {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ContractsAsciidocGenerator.class);
+
+  private final static String DOC_FORMAT = "asciidoc";
+
+  @Override
+  public boolean canProcess(String type) {
+    return type != null && DOC_FORMAT.equals(type.toLowerCase());
+  }
+
+  @Override
+  public String generate(Swagger contractContent, String outputPath) {
+
+    Asciidoctor asciidoctor = Asciidoctor.Factory.create();
+    Swagger2MarkupConverter.Builder markupBuilder = Swagger2MarkupConverter.from(contractContent);
+    Swagger2MarkupConfigBuilder swagger2MarkupConfigBuilder = new Swagger2MarkupConfigBuilder()
+        .withParameterOrdering(Ordering
+            .explicit("path", "query", "header", "cookie", "formData", "body")
+            .onResultOf(Parameter::getIn));
+    String markup = markupBuilder.withConfig(swagger2MarkupConfigBuilder.build()).build().toString();
+
+    final Map<String, Object> optionsMap = OptionsBuilder.options()
+        .docType("book")
+        .backend("html5")
+        .headerFooter(true)
+        .safe(SafeMode.UNSAFE)
+        .attributes(AttributesBuilder.attributes()
+            .attribute("toclevels", 3)
+            .attribute(Attributes.TOC_2, true)
+            .attribute(Attributes.TOC_POSITION, "left")
+            .attribute(Attributes.LINK_CSS, true)
+            .attribute(Attributes.SECTION_NUMBERS, true)
+            .attribute(Attributes.SECT_NUM_LEVELS, 4))
+        .asMap();
+    String asciidocResult = asciidoctor.convert(markup, optionsMap);
+    try {
+      Files.write(Paths.get(outputPath), asciidocResult.getBytes());
+    } catch (IOException e) {
+      LOGGER.error(e.getMessage());
+    }
+
+    return asciidocResult;
+  }
+}
---
+++ doc-generator/src/main/java/org/apache/servicecomb/docgen/ContractsSwaggerUIGenerator.java
@@ -0,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.servicecomb.docgen;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import io.swagger.models.Swagger;
+import io.swagger.util.Json;
+
+public class ContractsSwaggerUIGenerator implements DocGenerator {
+
+  private static Pattern variablePattern = Pattern.compile("(?<=\\{\\{)[a-zA-Z0-9_-]*(?=\\}\\})");
+
+  private final static String DOC_FORMAT = "html";
+
+  @Override
+
+  public boolean canProcess(String type) {
+    return DOC_FORMAT.equals(type);
+  }
+
+  @Override
+  public String generate(Swagger contractContent, String outputPath) {
+
+    String swaggerUiHtml = null;
+    try {
+      String swaggerJson = Json.mapper().writeValueAsString(contractContent);
+      swaggerUiHtml = renderHtml(getSwaggerUiHtml(), Collections.singletonMap("spec", swaggerJson));
+
+      outputPath = correctPath(outputPath);
+      Path outputFile = Paths.get(outputPath);
+
+      if (!Files.exists(outputFile)) {
+        if (outputFile.getParent() != null) {
+          Files.createDirectories(outputFile.getParent());
+        }
+        Files.createFile(outputFile);
+      }
+
+      Files.write(outputFile, swaggerUiHtml.getBytes());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return swaggerUiHtml;
+  }
+
+  private String correctPath(String filepath) {
+
+    if (!filepath.endsWith("." + DOC_FORMAT)) {
+      return filepath + DOC_FORMAT;
+    }
+
+    return filepath;
+  }
+
+  private String renderHtml(String html, Map<String, String> args) {
+
+    Matcher variableMatcher = variablePattern.matcher(html);
+    while (variableMatcher.find()) {
+      String variableStr = variableMatcher.group();
+      String variableValue = args.get(variableStr);
+
+      html = html.replace("{{" + variableStr + "}}", variableValue);
+    }
+
+    return html;
+  }
+
+  private String getSwaggerUiHtml() throws IOException {
+    int len;
+
+    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+    InputStream in = this.getClass().getClassLoader().getResourceAsStream("webroot/swagger-ui.html");
+
+    byte[] buf = new byte[1024];
+    while ((len = in.read(buf)) != -1) {
+      bout.write(buf, 0, len);
+    }
+    bout.close();
+    in.close();
+
+    return new String(bout.toByteArray());
+  }
+}
